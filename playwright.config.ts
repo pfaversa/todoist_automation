6614ebe6e79b288e22a9f2bc14192dd1b2db7f61
Pablo Aversa <pablo.aversa@Pablos-MacBook-Pro.local>
2022-11-24 16:19:18 -0600

    config run test command and default playwritgh configuration

---
+++ playwright.config.ts
@@ -0,0 +1,33 @@
+import { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+    timeout: 60000,
+    retries: 0,
+    use: {
+        headless: false,
+        //viewport:
+        actionTimeout: 15000,
+        video: 'off',
+        screenshot: 'off',
+    },
+    projects: [
+        {
+            name: 'Chromium',
+            use:{browserName: 'chromium'},
+        },
+        {
+            name: 'Firefox',
+            use:{browserName: 'firefox'},
+        },
+        {
+            name: 'Webkit',
+            use: {browserName: 'webkit'},
+        },
+    ],
+}
+
+export default config
+
+//npx playwright test --config=playwright.config.ts --project=Chromium --reporter=line
+//npx playwright test --config=playwright.config.ts --project=Chromium --reporter=html
+// npx playwright show-report
